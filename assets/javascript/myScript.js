63dfe3b29947d2b56b9e21a91287f648b4c299ff
MarisKX <maris.kurpnieks@gmail.com>
2021-10-22 17:01:42 +0000

    Added core content of index page + javascript functionality for random car selector and popup window generator/filler

--- assets/javascript/myScript.js
+++ assets/javascript/myScript.js
@@ -25,3 +25,45 @@ function displayMenu(){
         mainNavFrame.style.display = "block";
     }
 }
+
+// selects random cars for sale to display on index page -----------------------------------
+
+let carSelectorLeftSale = Math.floor(Math.random() * $(".cars-s-l").length);
+$(".cars-s-l").hide().eq(carSelectorLeftSale).show();
+
+let carSelectorMiddleSale = Math.floor(Math.random() * $(".cars-s-m").length);
+$(".cars-s-m").hide().eq(carSelectorMiddleSale).show();
+
+let carSelectorRightSale = Math.floor(Math.random() * $(".cars-s-r").length);
+$(".cars-s-r").hide().eq(carSelectorRightSale).show();
+
+let carSelectorLeftRent = Math.floor(Math.random() * $(".cars-r-l").length);
+$(".cars-r-l").hide().eq(carSelectorLeftRent).show();
+
+let carSelectorMiddleRent = Math.floor(Math.random() * $(".cars-r-m").length);
+$(".cars-r-m").hide().eq(carSelectorMiddleRent).show();
+
+let carSelectorRightRent = Math.floor(Math.random() * $(".cars-r-r").length);
+$(".cars-r-r").hide().eq(carSelectorRightRent).show();
+
+// shows car details from index page and adds image src to newly opened pop-up ---------------------------------------------------
+
+function showCarDetails(){
+  let show = document.querySelector(".car-list-item");
+  let hide = document.querySelector(".main-content-section");
+  let imgSrc = $(".item-image-small").attr("src");
+  console.log(imgSrc);
+  $("#first-image").attr("src", imgSrc);
+  show.classList.toggle("show");
+  hide.classList.toggle("hide");
+}
+
+// hides car details from index page (returns back to default index screen)-----------------------------------------------------
+
+function hideCarDetails(){
+  let show = document.querySelector(".car-list-item");
+  let hide = document.querySelector(".main-content-section");
+
+  show.classList.remove("show");
+  hide.classList.remove("hide");
+}
