7a5fcc6b4cc0dee5ae33ea9fcd6c639fb4f007b4
MarisKX <maris.kurpnieks@gmail.com>
2021-10-27 00:13:18 +0000

    Filled cars for rent section, including javascript adjustments and minor bug fixes (pop up displays middle of content instead of top, carousel images don't go back to default picture and other)

--- assets/javascript/myScript.js
+++ assets/javascript/myScript.js
@@ -37,45 +37,43 @@ $(".cars-s-m").hide().eq(carSelectorMiddleSale).show().addClass("random-car-sale
 let carSelectorRightSale = Math.floor(Math.random() * $(".cars-s-r").length);
 $(".cars-s-r").hide().eq(carSelectorRightSale).show().addClass("random-car-sale");
 
+// selects random cars for rent to display on index page -----------------------------------
+
 let carSelectorLeftRent = Math.floor(Math.random() * $(".cars-r-l").length);
-$(".cars-r-l").hide().eq(carSelectorLeftRent).show();
+$(".cars-r-l").hide().eq(carSelectorLeftRent).show().addClass("random-car-sale");
 
 let carSelectorMiddleRent = Math.floor(Math.random() * $(".cars-r-m").length);
-$(".cars-r-m").hide().eq(carSelectorMiddleRent).show();
+$(".cars-r-m").hide().eq(carSelectorMiddleRent).show().addClass("random-car-sale");
 
 let carSelectorRightRent = Math.floor(Math.random() * $(".cars-r-r").length);
-$(".cars-r-r").hide().eq(carSelectorRightRent).show();
+$(".cars-r-r").hide().eq(carSelectorRightRent).show().addClass("random-car-sale");
 
-
+// adjusts clases for smaller displays -----------------------------------------------------------
 
 if($(this).width() <1450){
   $("#third-car").addClass("hidden");
+  $("#third-car-rent").addClass("hidden");
   $(".cars-for-sale .col-md-4").each(function(){
+    $(this).removeClass("col-md-4").addClass("col-md-6")
+  });
+  $(".cars-for-rent .col-md-4").each(function(){
     $(this).removeClass("col-md-4").addClass("col-md-6")
   });
 }
 
 if($(this).width() <950){
   $("#third-car").removeClass("hidden");
+  $("#third-car-rent").removeClass("hidden");
   $(".cars-for-sale .col-md-6").each(function(){
+    $(this).removeClass("col-md-6").addClass("col-md-12")
+  });
+  $(".cars-for-rent .col-md-6").each(function(){
     $(this).removeClass("col-md-6").addClass("col-md-12")
   });
 }
 
 
-// locations map on index page --------------------------------------------------------
-
-// function initMap() {
-//  let googleMap = new google.maps.Map(document.getElementById("location-map"), {
-//    zoom: 3,
-//    center: {
-//      lat: 51.585255,
-//      lng: 5.056375
-//    }
-//  });
-//}
-
-// shows car details from index page ---------------------------------------------------
+// shows car details from index page (SALE)---------------------------------------------------
 
 function showCarDetailsSl(){
   let show = document.querySelector(".car-list-item");
@@ -116,6 +114,7 @@ function showCarDetailsSl(){
   $("#right-side-description-p").text(descriptionRight);
   show.classList.toggle("show");
   hide.classList.toggle("hide");
+  window.scrollTo(0,0);
 }
 
 function showCarDetailsSm(){
@@ -157,6 +156,7 @@ function showCarDetailsSm(){
   $("#right-side-description-p").text(descriptionRight);
   show.classList.toggle("show");
   hide.classList.toggle("hide");
+  window.scrollTo(0,0);
 }
 
 function showCarDetailsSr(){
@@ -198,22 +198,92 @@ function showCarDetailsSr(){
   $("#right-side-description-p").text(descriptionRight);
   show.classList.toggle("show");
   hide.classList.toggle("hide");
+  window.scrollTo(0,0);
+}
+
+// shows car details from index page (RENT)---------------------------------------------------
+
+function showCarDetailsRl(){
+  let show = document.querySelector(".car-list-item-rent");
+  let hide = document.querySelector(".main-content-section");
+  let imgSrcFirst = $(".cars-r-l.random-car-sale").find(".first-image-small").attr("src");
+  let imgSrcSecond = $(".cars-r-l.random-car-sale").find(".second-image-small").attr("src");
+  let imgSrcThird = $(".cars-r-l.random-car-sale").find(".third-image-small").attr("src");
+  let imgSrcFourth = $(".cars-r-l.random-car-sale").find(".fourth-image-small").attr("src");
+  let make = $(".cars-r-l.random-car-sale").find(".make").text();
+  let model = $(".cars-r-l.random-car-sale").find(".model").text();
+  let year = $(".cars-r-l.random-car-sale").find(".year").text();
+  let fuel = $(".cars-r-l.random-car-sale").find(".fuel").text();
+  let engine = $(".cars-r-l.random-car-sale").find(".engine").text();
+  let gearbox = $(".cars-r-l.random-car-sale").find(".gearbox").text();
+  let milage = $(".cars-r-l.random-car-sale").find(".milage").text();
+  let apk = $(".cars-r-l.random-car-sale").find(".apk").text();
+  let color = $(".cars-r-l.random-car-sale").find(".color").text();
+  let price = $(".cars-r-l.random-car-sale").find(".price").text();
+  let descriptionLeft = $(".cars-r-l.random-car-sale").find(".description-first").text();
+  let descriptionMiddle = $(".cars-r-l.random-car-sale").find(".description-second").text();
+  let descriptionRight = $(".cars-r-l.random-car-sale").find(".description-third").text();
+  $("#first-image-details-rent").attr("src", imgSrcFirst);
+  $("#second-image-details-rent").attr("src", imgSrcSecond);
+  $("#third-image-details-rent").attr("src", imgSrcThird);
+  $("#fourth-image-details-rent").attr("src", imgSrcFourth);
+  $("#make").text(make);
+  $("#model").text(model);
+  $("#year").text(year);
+  $("#fuel").text(fuel);
+  $("#engine").text(engine);
+  $("#gearbox").text(gearbox);
+  $("#milage").text(milage);
+  $("#apk").text(apk);
+  $("#color").text(color);
+  $("#price").text(price);
+  $("#left-side-description-p").text(descriptionLeft);
+  $("#middle-side-description-p").text(descriptionMiddle);
+  $("#right-side-description-p").text(descriptionRight);
+  show.classList.toggle("show");
+  hide.classList.toggle("hide");
+  window.scrollTo(0,0);
 }
 
 // hides car details from index page (returns back to default index screen)-----------------------------------------------------
 
-function hideCarDetails(){
+function hideCarDetailsSale(){
   let show = document.querySelector(".car-list-item");
   let hide = document.querySelector(".main-content-section");
-
+  let currentPicture = document.querySelector(".active");
   show.classList.remove("show");
   hide.classList.remove("hide");
+  currentPicture.classList.remove("active");
+  $("#first-sale").addClass("active");
 }
 
-// moves to next and previous pictures in popup window ----------------------------------------------------------------------
-
-function nextCarouselItem(){
+function hideCarDetailsRent(){
+  let show = document.querySelector(".car-list-item-rent");
+  let hide = document.querySelector(".main-content-section");
   let currentPicture = document.querySelector(".active");
+  show.classList.remove("show");
+  hide.classList.remove("hide");
+  currentPicture.classList.remove("active");
+  $("#first-rent").addClass("active");
+}
+
+// locations map on index page --------------------------------------------------------
+
+// function initMap() {
+//  let googleMap = new google.maps.Map(document.getElementById("location-map"), {
+//    zoom: 3,
+//    center: {
+//      lat: 51.585255,
+//      lng: 5.056375
+//    }
+//  });
+//}
+
+// moves to next and previous pictures in popup window SALE ----------------------------------------------------------------------
+
+function nextCarouselItemSale(){
+  let carouselType = document.querySelector(".car-list-item");
+  let currentPicture = carouselType.querySelector(".active");
   let nextPicture = currentPicture.nextElementSibling;
   if(nextPicture == null){
     return;
@@ -223,8 +293,34 @@ function nextCarouselItem(){
   }
 }
 
-function previousCarouselItem(){
+function previousCarouselItemSale(){
   let currentPicture = document.querySelector(".active");
+  let previousPicture = currentPicture.previousElementSibling;
+  if (previousPicture == null){
+    return;
+    } else {
+    currentPicture.classList.remove("active");
+    previousPicture.classList.add("active");
+  }
+}
+
+// moves to next and previous pictures in popup window RENT ----------------------------------------------------------------------
+
+function nextCarouselItemRent(){
+  let carouselType = document.querySelector(".car-list-item-rent");
+  let currentPicture = carouselType.querySelector(".active");
+  let nextPicture = currentPicture.nextElementSibling;
+  if(nextPicture == null){
+    return;
+    } else {
+    currentPicture.classList.remove("active");
+    nextPicture.classList.add("active");
+  }
+}
+
+function previousCarouselItemRent(){
+  let carouselType = document.querySelector(".car-list-item-rent");
+  let currentPicture = carouselType.querySelector(".active");
   let previousPicture = currentPicture.previousElementSibling;
   if (previousPicture == null){
     return;
@@ -237,7 +333,7 @@ function previousCarouselItem(){
 // shows car details on cars for sale and rent page -----------------------------------------------------------------------------------
 
 
-function clickedCar(carsId) {
+function clickedCarSale(carsId) {
   console.log (carsId);
   showCarDetails();
   function showCarDetails() {
@@ -279,5 +375,52 @@ function clickedCar(carsId) {
     $("#right-side-description-p").text(descriptionRight);
     show.classList.toggle("show");
     hide.classList.toggle("hide");
+    window.scrollTo(0,0);
+  }
+}
+
+function clickedCarRent(carsId) {
+  console.log (carsId);
+  showCarDetails();
+  function showCarDetails() {
+    let show = document.querySelector(".car-list-item-rent");
+    let hide = document.querySelector(".main-content-section");
+    let imgSrcFirst = $("#" + carsId).find(".first-image-small").attr("src");
+    let imgSrcSecond = $("#" + carsId).find(".second-image-small").attr("src");
+    let imgSrcThird = $("#" + carsId).find(".third-image-small").attr("src");
+    let imgSrcFourth = $("#" + carsId).find(".fourth-image-small").attr("src");
+    let make = $("#" + carsId).find(".make").text();
+    let model = $("#" + carsId).find(".model").text();
+    let year = $("#" + carsId).find(".year").text();
+    let fuel = $("#" + carsId).find(".fuel").text();
+    let engine = $("#" + carsId).find(".engine").text();
+    let gearbox = $("#" + carsId).find(".gearbox").text();
+    let milage = $("#" + carsId).find(".milage").text();
+    let apk = $("#" + carsId).find(".apk").text();
+    let color = $("#" + carsId).find(".color").text();
+    let price = $("#" + carsId).find(".price").text();
+    let descriptionLeft = $("#" + carsId).find(".description-first").text();
+    let descriptionMiddle = $("#" + carsId).find(".description-second").text();
+    let descriptionRight = $("#" + carsId).find(".description-third").text();
+    $("#first-image-details-rent").attr("src", imgSrcFirst);
+    $("#second-image-details-rent").attr("src", imgSrcSecond);
+    $("#third-image-details-rent").attr("src", imgSrcThird);
+    $("#fourth-image-details-rent").attr("src", imgSrcFourth);
+    $("#make").text(make);
+    $("#model").text(model);
+    $("#year").text(year);
+    $("#fuel").text(fuel);
+    $("#engine").text(engine);
+    $("#gearbox").text(gearbox);
+    $("#milage").text(milage);
+    $("#apk").text(apk);
+    $("#color").text(color);
+    $("#price").text(price);
+    $("#left-side-description-p").text(descriptionLeft);
+    $("#middle-side-description-p").text(descriptionMiddle);
+    $("#right-side-description-p").text(descriptionRight);
+    show.classList.toggle("show");
+    hide.classList.toggle("hide");
+    window.scrollTo(0,0);
   }
 }
